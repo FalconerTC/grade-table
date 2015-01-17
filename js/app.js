cf9b993e4e22d0ad8ad65de29e4322341eeea53e
Falcon Taylor-Carter <falconertc@colorado.edu>
2015-01-17 16:53:19 -0700

    Added highlighting for low grade values

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
 	  {name: "Jacob", grade: 27},
 	  {name: "Nephi", grade: 29},
 	  {name: "Enos", grade: 34}
- ];
+	];
 
   app.controller('TableController', function($scope, $localStorage) {
 
@@ -18,10 +18,20 @@
   	$scope.gridOptions = {};
 
   	$scope.gridOptions.columnDefs = [
-  		{ name: 'name', displayName: 'Name' },
-  		{ name: 'grade', displayName: 'Grade', type: 'number'}
+  		{ field: 'name', displayName: 'Name', 
+  			cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+  				console.log(row);
+  				if (grid.getCellValue(row, col ) < 50)
+  					return 'failure';
+  			}
+  		},
+  		{ field: 'grade', displayName: 'Grade', type: 'number', 
+  			cellClass: function(grid, row, col, rowRenderIndex, colRenderIndex) {
+  				if (grid.getCellValue(row, col) < 50)
+  					return 'failure';
+  			}
+  		}
   	];
-
 
   	$scope.gridOptions.data = $scope.storage.users;
 
