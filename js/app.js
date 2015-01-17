2e1fd823b273954b540352e3d30b1f2db4ea6722
Falcon Taylor-Carter <falconertc@colorado.edu>
2015-01-17 15:45:40 -0700

    Basic grid using local storage

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 (function() {
-  var app = angular.module('grade-table', []);
+  var app = angular.module('grade-table', ['ui.grid', 'ui.grid.edit', 'ngStorage']);
 
+  var defaultUsers = [
+	  {name: "Moroni", grade: 50},
+	  {name: "Tiancum", grade: 43},
+	  {name: "Jacob", grade: 27},
+	  {name: "Nephi", grade: 29},
+	  {name: "Enos", grade: 34}
+ ];
+
+  app.controller('TableController', function($scope, $localStorage) {
+
+  	$scope.storage = $localStorage.$default({
+  		users: defaultUsers
+  	});
+
+  	$scope.gridOptions = {};
+
+  	$scope.gridOptions.columnDefs = [
+  		{ name: 'name', displayName: 'Name' },
+  		{ name: 'grade', displayName: 'Grade', type: 'number'}
+  	];
+
+
+  	$scope.gridOptions.data = $scope.storage.users;
+
+  });
 
 })();
